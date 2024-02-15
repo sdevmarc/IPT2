98cdf05afe8accd414b485a418f9a095f65d96fd
sdevmarc <suanieeee@yahoo.com>
2024-02-15 21:47:30 +0800

    Removed w-full an h screen

--- Student Information System/dashboard/src/pages/Dashboard.js
+++ Student Information System/dashboard/src/pages/Dashboard.js
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar'
 const Dashboard = () => {
     return (
         <>
-            <div className="w-full h-screen flex">
+            <div className="flex">
                 <Sidebar />
                 <div className="h-screen flex p-4">
                     <h1 className='font-bold text-[23px] text-[#222]'>Welcome to Saint Mary's University</h1>
