79b779811dde29d70fec33f8948b299d07004d23
Marc Edison <suanieeee@yahoo.com>
2024-02-17 22:45:11 +0800

    Added edit function

--- Student Information System/dashboard/src/pages/ViewStudents.js
+++ Student Information System/dashboard/src/pages/ViewStudents.js
@@ -22,8 +22,8 @@ function ViewStudents() {
         year: ''
     })
 
-    useEffect(async () => {
-        await axios.get(`http://localhost:1337/viewstudents`)
+    useEffect(() => {
+        axios.get(`http://localhost:1337/viewstudents`)
             .then((response) => {
                 setStudents(response.data)
             })
@@ -45,12 +45,17 @@ function ViewStudents() {
         setIsOpen(true)
     }
     function handleClose() {
-        // axios.post('http://localhost:1337/editstudent', values)
-        //     .then(res => alert('Edit Successful!'))
-        //     .catch(err => console.log(`Error: ${err}`))
+        axios.post('http://localhost:1337/editstudent', values)
+            .then(() => {
+                alert('Edit Successful!')
+                setValues({ ...values, id: '', lastname: '', firstname: '', middlename: '', course: '', year: '' })
+                setIsOpen(false)
+                window.location.reload()
+            }
+            )
+            .catch(err => console.log(`Error: ${err}`))
 
-        setValues({ ...values, id: '', lastname: '', firstname: '', middlename: '', course: '', year: '' })
-        setIsOpen(false)
+
 
     }
 
@@ -119,36 +124,36 @@ function ViewStudents() {
                                     ))}
                                 </TableBody>
                             </Table>
+
                             <Modal open={isOpen} onClose={handleClose}>
-
-                                <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] overflow-auto flex flex-col justify-start gap-4 w-[40rem] h-[40rem] bg-white p-10">
-                                    <div className="flex flex-col gap-3">
-                                        <TextField value={values.id} onChange={handleOnChangeId} label="ID Number" id="outlined-basic" variant="outlined" />
-                                        <TextField value={values.lastname} onChange={handleOnChangeLastname} label="Last Name" id="outlined-basic" variant="outlined" />
-                                        <TextField value={values.firstname} onChange={handleOnChangeFirstname} label="First Name" id="outlined-basic" variant="outlined" />
-                                        <TextField value={values.middlename} onChange={handleOnChangeMiddlename} label="Middle Name" id="outlined-basic" variant="outlined" />
-                                        <TextField value={values.course} onChange={handleOnChangeCourse} label="Course" id="outlined-basic" variant="outlined" />
-                                        <TextField value={values.year} onChange={handleOnChangeYear} label="Year" id="outlined-basic" variant="outlined" />
+                                <form onSubmit={handleClose}>
+                                    <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] overflow-auto flex flex-col justify-start gap-4 w-[40rem] h-[40rem] bg-white p-10">
+                                        <div className="flex flex-col gap-3">
+                                            <TextField value={values.id} onChange={handleOnChangeId} label="ID Number" id="outlined-basic" variant="outlined" />
+                                            <TextField value={values.lastname} onChange={handleOnChangeLastname} label="Last Name" id="outlined-basic" variant="outlined" />
+                                            <TextField value={values.firstname} onChange={handleOnChangeFirstname} label="First Name" id="outlined-basic" variant="outlined" />
+                                            <TextField value={values.middlename} onChange={handleOnChangeMiddlename} label="Middle Name" id="outlined-basic" variant="outlined" />
+                                            <TextField value={values.course} onChange={handleOnChangeCourse} label="Course" id="outlined-basic" variant="outlined" />
+                                            <TextField value={values.year} onChange={handleOnChangeYear} label="Year" id="outlined-basic" variant="outlined" />
+                                        </div>
+                                        <div className="flex flex-col gap-3">
+                                            {/* <h2 className='text-[25px] mb-5'>ID NUMBER: <span className='font-bold'>{student ? `${student.id}` : null}</span></h2> */}
+                                            <h2>STUDENT ID: <span className='font-bold'>{values.id}</span></h2>
+                                            <h2>LAST NAME: <span className='font-bold'>{values && `${values.lastname}`}</span></h2>
+                                            <h2>FIRST NAME: <span className='font-bold'>{values && `${values.firstname}`}</span></h2>
+                                            <h2>MIDDLE NAME: <span className='font-bold'>{values && `${values.middlename}`}</span></h2>
+                                            <h2>COURSE: <span className='font-bold'>{values && `${values.course}`}</span></h2>
+                                            <h2 className='mb-5'>YEAR: <span className='font-bold'>{values && `${values.year}`}</span></h2>
+                                        </div>
+                                        <Button variant='contained' type='submit'>Close</Button>
                                     </div>
-
-
-                                    <div className="flex flex-col gap-3">
-                                        {/* <h2 className='text-[25px] mb-5'>ID NUMBER: <span className='font-bold'>{student ? `${student.id}` : null}</span></h2> */}
-                                        <h2>STUDENT ID: <span className='font-bold'>{values.id}</span></h2>
-                                        <h2>LAST NAME: <span className='font-bold'>{values && `${values.lastname}`}</span></h2>
-                                        <h2>FIRST NAME: <span className='font-bold'>{values && `${values.firstname}`}</span></h2>
-                                        <h2>MIDDLE NAME: <span className='font-bold'>{values && `${values.middlename}`}</span></h2>
-                                        <h2>COURSE: <span className='font-bold'>{values && `${values.course}`}</span></h2>
-                                        <h2 className='mb-5'>YEAR: <span className='font-bold'>{values && `${values.year}`}</span></h2>
-                                    </div>
-
-                                    <Button variant='contained' onClick={handleClose}>Close</Button>
-                                </div>
+                                </form>
                             </Modal>
+
                         </div>
                     </div>
-                </div>
-            </div>
+                </div >
+            </div >
         </>
     )
 }
